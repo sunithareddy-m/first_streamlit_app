ede56796a92befa1a7914f72f6afb2590cd07f85
sunithareddy-m <111298365+sunithareddy-m@users.noreply.github.com>
2022-08-16 15:49:56 +0530

    Update streamlit_app.py
    multi select option has been changed from fruit number to fruit name by changing the index(set_index)

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so that they can pick the fruit they want to include
 streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
