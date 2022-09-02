fad6a17ccef4396b4b7cbcda198d9925746cbd5b
sunithareddy-m <111298365+sunithareddy-m@users.noreply.github.com>
2022-09-02 22:54:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ def get_fruit_load_list():
        return my_cur.fetchall()
     
 #Add a button to load the fruit
-if stremlit.button('get fruit_load_list'):
+if stremlit.button('get_fruit_load_list'):
  
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
